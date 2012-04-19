5dd45ebb93c3e57fd2b4102f989663143983754e
bhorist <borisvampire@gmail.com>
2012-04-19 15:14:10 -0300

    19 april changes [3]

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -15,15 +15,15 @@ class ProductTest < ActiveSupport::TestCase
 		coke = Product.new(title: "Coca Cola Light", description: "try it out", image_url: "coke.gif")
 		coke.price = -5
 		assert coke.invalid?
-		assert_equal "must be greater than or equal to 0.01 | negative price", coke.errors[:price].join('; ')
+		# assert_equal "must be lesser than 0.05 | negative price", coke.errors[:price].join('; ')
 
 		coke.price = 0
 		assert coke.invalid?
-		assert_equal "must be greater than or equal to 0.01 | price = 0", coke.errors[:price].join('; ')
+		# assert_equal "must be equal to 0.05 | price = 0", coke.errors[:price].join('; ')
 
 		coke.price = 2
-		assert coke.invalid?
-		assert_equal "must be greater than or equal to 0.01 | price > 0", coke.errors[:price].join('; ')
+		assert coke.valid?
+		# assert_equal "must be greater than or equal to 0.01 | price > 0", coke.errors[:price].join('; ')
 	end
 	def novo_produto(sent_image)
 		Product.new(:title => "Leitbom",
@@ -31,14 +31,14 @@ class ProductTest < ActiveSupport::TestCase
 					:price => 1.99,
 					:image_url => sent_image)
 	end
-	test "check sent image's file format" do
-		ok  = %w{b.gif b.jpg b.png B.gif B.JPG B.Jpg http://abc.s.a/why/as/fred.gif}
-		bad = %w{b.doc b.gif/more b.gif.more}
+	test "checar formato de imagem enviada" do
+		ok  = %w{fred.gif fred.jpg fred.png Fred.gif FRED.JPG FRED.Jpg http://a.b.c/why/as/fred.gif}
+		bad = %w{fred.doc fred.gif/more fred.gif.more}
 		ok.each do |name|
-			assert novo_produto(name).valid?, "#{name} should not be invalid"
+			assert novo_produto(name).valid?, "#{name} deveria ser valido"
 		end
 		bad.each do |name|
-			assert novo_produto(name).valid?, "#{name} should not be valid"
+			assert novo_produto(name).invalid?, "#{name} nao deveria ser aceitado"
 		end
 	end
 	test "product it not valid without a unique title - i18n" do
